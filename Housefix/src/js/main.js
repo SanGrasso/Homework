d67f586feaefc139fa8a0469aa43cbcd78201a04
SanGrasso <143331218+SanGrasso@users.noreply.github.com>
2023-09-18 15:53:49 +0200

    Housefix

---
+++ Housefix/src/js/main.js
@@ -0,0 +1,59 @@
+// var button = document.querySelector('#button');
+// var modal = document.querySelector('#modal');
+// var close = document.querySelector('#close');
+
+// button.addEventListener('click', function(){
+//     modal.classList.add('modal_active')
+// });
+
+// close.addEventListener('click', function(){
+//     modal.classList.remove('modal_active')
+// });
+
+$(document).ready(function(){
+
+    //Napojení knihovny wow.js
+    new WOW().init();
+
+    var button = $('#button');
+    var modal = $('#modal');
+    var close = $('#close');
+
+    button.on('click', function(){
+        modal.addClass('modal_active');
+    });
+    close.on('click', function(){
+        modal.removeClass('modal_active');
+    });
+
+    //Napojení slideru
+    $('.slider').slick({
+        slidesToShow: 3,
+        slideToScroll: 3,
+        prevArrow: $('.arrows__left'),
+        nextArrow: $('.arrows__right'),
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1,
+                }
+              },
+           
+         
+          ]
+
+
+
+
+    });
+
+
+
+});
